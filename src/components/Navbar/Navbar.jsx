d3ccc7154f656dc13a0e44d503e7d69abae7e776
AndyRCR <andycr321@gmail.com>
2023-05-17 15:06:45 -0500

    HTML > new logo blink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,39 +4,47 @@ import { GlobalContext } from '../../context/GlobalStateContext'
 import './Navbar.css'
 
 const Navbar = () => {
+	const { windowHeight } = useContext(GlobalContext)
 
-  const {windowHeight} = useContext(GlobalContext)
+	const [scroll, setScroll] = useState(0)
 
-  const [scroll, setScroll] = useState(0)
+	const navigate = useNavigate()
 
-  const navigate = useNavigate()
+	const handleScroll = () => setScroll(window.scrollY)
 
-  const handleScroll = () => setScroll(window.scrollY)
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
 
-    return () =>{
-        window.removeEventListener('scroll', handleScroll)
-    }
+		// eslint-disable-next-line
+	}, [scroll])
 
-    // eslint-disable-next-line
-}, [scroll])
-
-  return (
-    <div className={scroll < 10 ? 'navbar' : 'navbar navbarTransparent'}>
-        <div className='logo'>
-          {windowHeight < 500 ? 'b' : 'blink'}
-        </div>
-        <button
-        onClick={() => {
-          document.body.style.overflowY = 'auto'
-          navigate('/')}}
-        className='secondaryButton'>
-          Asesores
-        </button>
-    </div>
-  )
+	return (
+		<div className={scroll < 10 ? 'navbar' : 'navbar navbarTransparent'}>
+			<div className='logo'>
+				{windowHeight < 500 ? (
+					'b'
+				) : (
+					<img
+						src='https://i.ibb.co/9hnvNdf/blinklogo-removebg-preview.png'
+						alt='logo blink'
+					/>
+				)}
+			</div>
+			<button
+				onClick={() => {
+					document.body.style.overflowY = 'auto'
+					navigate('/')
+				}}
+				className='secondaryButton'
+			>
+				Asesores
+			</button>
+		</div>
+	)
 }
 
 export default Navbar
